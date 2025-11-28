8432c43fb601aa03d22c56140399883c5ba5ff04
Aliya.k <aliyakhoerunisa05@gmail.com>
2025-11-28 13:49:17 +0700

    Add files via upload

---
+++ website-company-profile/inc/inc_koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host     = "localhost";
+$user     = "root";
+$pass     = "";
+$db       = "companyprofile";
+
+$koneksi  = mysqli_connect($host,$user,$pass,$db);
+if(!$koneksi){
+    die("gagal terkoneksi");
+} else {
+    echo "koneksi berhasil";
+}
+ 
